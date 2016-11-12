091df0693418d024cf0dbd6a12815791fa5434b8
Geoffrey Ostosh <ostoshg@gmail.com>
2016-11-12 13:48:20 -0800

    add string builder implementation

---
+++ StringBuilder.js
@@ -0,0 +1,38 @@
+((function(wwindow) {
+  /**
+  * Creates new {@link StringBuilder} instances.
+  *
+  * @constructor
+  */
+  var StringBuilder = function() {
+    /** @private {boolean} */
+    this._isBuilt = false;
+
+    /** @private {integer} */
+    this._index = -1;
+
+    /** @private {Array} */
+    this._content = [];
+  };
+
+  /**
+  * Append value to StringBuilder instance. 
+  *
+  * @param {object} value The value to be appended.
+  */
+  StringBuilder.prototype.append = function( value ) {
+    this.content[++this._index] = value;
+  };
+  
+
+  /**
+  * Returns string representation of String Builder instance 
+  *
+  */
+  StringBuilder.prototype.toString = function() {
+    return this.content.join('');
+  };
+
+  window.harness = StringBuilder;
+
+}(window)))
